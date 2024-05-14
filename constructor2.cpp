a94f9ea40baa8f89b94a14756ebf20b21928f733
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-14 15:37:14 +0700

    membuat fungsi main

--- constructor2.cpp
+++ constructor2.cpp
@@ -35,4 +35,18 @@ class mahasiswa{
 
 };
 
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("indra");
+    mahasiswa mhs4(30, "Fauzan");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
 
