442e195dbb4ea32814b60bab489c79d56c8a7ca9
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-05-14 15:03:22 +0700

    membuat class mahasiswa

---
+++ mbrStaticfunc.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class mahasiswa{
+    private:
+        static int nim;
+
+    public:
+        int id;
+        string nama;
+
+        void setID();
+
+        void printAll();
+
+        static void setNim(int pNim){ nim = pNim; }
+        static int getNim(){ return nim; }
+
+        mahasiswa(string pnama)
+        {
+            nama=pnama;
+            setID();
+        
+        }
+};
